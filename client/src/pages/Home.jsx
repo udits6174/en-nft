f7cd849c197363485944bbbccbc50da50ffebaa6
udits6174 <udits502@gmail.com>
2024-04-22 14:58:33 +0530

    Build render

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
+import "../App.css";
+import Ticket from "../assets/Ticket.png";
+import General from "../assets/general_pass.png";
 
 const Home = () => {
   const location = useLocation();
@@ -8,18 +11,23 @@ const Home = () => {
   const revealMsg = async () => {
     try {
       const _walletAddress = location.state.address;
-      const response = await axios.post(
-        "https://en-nft.vercel.app/members",
-        {
-          from: _walletAddress,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
+      const response = await axios
+        .post(
+          "https://en-nft.vercel.app/members",
+          {
+            from: _walletAddress,
           },
-        }
-      );
-
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        // .then((response) => {
+        //   let token = response.data.access; // Extract the token
+        //   localStorage.setItem("SavedToken", "Bearer " + token); // Save to localStorage
+        //   axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+        // });
       const data = response.data;
       if (data.status === 200) {
         navigateTo("/members");
@@ -32,8 +40,26 @@ const Home = () => {
   };
   return (
     <>
+      <div style={{ display: "flex", justifyContent: "space-even", gap: "150px" }}>
+        <div>
+          <img
+            src={Ticket}
+            style={{ width: "256px", height: "256px" }}
+            alt="Ticket"
+          />
+          <h2>EXCLUSIVE ACCESS TICKET</h2>
+        </div>
+        <div>
+          <img
+            src={General}
+            style={{ width: "256px", height: "256px" }}
+            alt="Ticket"
+          />
+          <h2>GENERAL ACCESS TICKET</h2>
+        </div>
+      </div>
       <span className="beautiful-sentence">
-        I have a secret message for holders of my NFT collection
+        Click below for exclusive members only page.
       </span>
       <br></br>
       <br></br>
--- client/src/pages/Member.jsx
+++ client/src/pages/Member.jsx
@@ -1,32 +1,63 @@
-import {useEffect } from "react"
-import io from 'socket.io-client';
-import {useNavigate} from "react-router-dom"
-import '../App.css'
-
+import { useEffect } from "react";
+import io from "socket.io-client";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+import NFT1 from "../assets/NFT1.png";
+import NFT2 from "../assets/NFT2.png";
+import NFT3 from "../assets/NFT3.png";
 const Member = () => {
   const navigateTo = useNavigate();
-  useEffect(()=>{
-    const socketInstance = io('https://en-nft.vercel.app');
+  useEffect(() => {
+    const socketInstance = io("https://en-nft.vercel.app");
     console.log("connect", socketInstance);
-    socketInstance.on('nftsUpdated', (data)=>{
+    socketInstance.on("nftsUpdated", (data) => {
       console.log(data.userNFTs);
-      if(data.userNFTs<1){
-        navigateTo('/');
+      if (data.userNFTs < 1) {
+        navigateTo("/");
       }
-    }
-      )
-      
-      return()=>{
-        socketInstance.disconnect()
-      }
-    },[navigateTo])
+    });
 
-    return (
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, [navigateTo]);
+
+  return (
+    <>
       <div className="members-only-page">
         <h1>Welcome to the Premium Members Only Page!</h1>
-        <p>This is some exclusive content for our premium members.</p>
+        <p>These are some exclusive NFTs for our premium members.</p>
       </div>
-    );
-}
+      <div
+        style={{ display: "flex", justifyContent: "space-between", padding:"50px" }}
+      >
+        <div>
+          <img
+            src={NFT1}
+            style={{ width: "150px", height: "200px" }}
+            alt="Ticket"
+          />
+          <h2>cryptopunks</h2>
+        </div>
+        <div>
+          <img
+            src={NFT2}
+            style={{ width: "150px", height: "200px" }}
+            alt="Ticket"
+          />
+          <h2> LNFTs by Dicaso</h2>
+        </div>
+        <div>
+          <img
+            src={NFT3}
+            style={{ width: "150px", height: "200px" }}
+            alt="Ticket"
+          />
+          <h2>nakamigos</h2>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Member;
